39083cc22d45f51e1f0994c021e977193d88976f
niiishahac <ranjusuman1462@gmail.com>
2026-02-18 23:46:18 +0530

    selection sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,13 +1,20 @@
 public class BubbleSort {
     public static void bubbleSort(int arr[]){
+        // optimized bubble sort
+        boolean swapped;
         for(int turn=0;turn<arr.length-1;turn++){
+            swapped=false;
             for(int j=0;j<arr.length-1-turn;j++){
                 if(arr[j]>arr[j+1]){
                     // swap
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swapped=true;
                 }
+            }
+            if(!swapped){
+                break;
             }
         }
     }
---
+++ selectionsort.java
@@ -0,0 +1,28 @@
+public class selectionsort {
+    public static void selectionSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int minIndex=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+            // swap
+            int temp=arr[minIndex];
+            arr[minIndex]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[]={5,4,1,3,2};
+        selectionSort(arr);
+        printArray(arr);
+    }
+    
+}
